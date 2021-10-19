e6c0dfcab1963a0d02287d75a28eb9b9533ceffe
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2021-10-20 01:01:12 +0400

    First commit

---
+++ DelegateTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DelegateTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string, string> test = Fullname;
+            Predicate<int> salary = Salary;
+            Action<string, string> favmusic = FavMusic;
+            Console.WriteLine(test("Gunel","Humbatova"));
+            Console.WriteLine(salary(1500));
+            favmusic("Jeferrson Airplane","White Rabbit");
+        }
+        public static string Fullname(string name,string surname) //func
+        {
+            return $"{name} {surname}";
+        }
+        public static bool Salary(int salary)  //predicate
+        {
+            return salary > 1000;
+        }
+        public static void FavMusic(string singer,string music)
+        {
+            Console.WriteLine($"{singer}-{music}");
+        }
+        
+    }
+}
